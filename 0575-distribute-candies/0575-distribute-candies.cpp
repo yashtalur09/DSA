b1ac83c48d2eaab2075586620b330e6589a9c3ac
Talur Yashwanth <taluryash4@gmail.com>
2026-07-17 18:53:04 +0530

    Time: 112 ms (44.56%), Space: 120.2 MB (40.23%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         int n=candyType.size();
-        int cnt=0;
-        set<int> st;
+        unordered_map<int,int> mpp;
         for(int i=0;i<n;i++){
-            st.insert(candyType[i]);
+            mpp[candyType[i]]++;
         }
-        if(st.size()==n/2 || st.size()>n/2) return n/2;
-        return st.size();
+        if(mpp.size()>=n/2) return n/2;
+        return mpp.size();
         
     }
 };
