41cef4471dd5af7c4d338fa3990caaf62e94eb78
Talur Yashwanth <taluryash4@gmail.com>
2026-07-22 15:33:59 +0530

    Time: 11 ms (53.64%), Space: 31.9 MB (86.63%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i=i+2){
+            sum+=nums[i];
+       }
+       return sum;
+        
+    }
+};
