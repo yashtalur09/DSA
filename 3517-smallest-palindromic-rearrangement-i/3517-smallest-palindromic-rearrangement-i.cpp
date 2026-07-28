b8b819d732e0163c68308b48453fbcdea9508e1a
Talur Yashwanth <taluryash4@gmail.com>
2026-07-28 10:15:35 +0530

    Time: 78 ms (41.32%), Space: 53.9 MB (98.46%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n=s.size();
+        if(n==1) return s;
+        int mid=n/2;
+        sort(s.begin(),s.begin()+mid);
+        for(int i=0;i<mid;i++){
+            s[n-i-1]=s[i];
+        }
+        return s;
+    }
+};
