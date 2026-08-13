8466d1da2e6b4ad58790991c1d099f7464fad20c
Talur Yashwanth <taluryash4@gmail.com>
2026-08-13 15:45:59 +0530

    Time: 0 ms (100%), Space: 17.6 MB (75.13%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,20 +1,25 @@
 class Solution {
-    int func(int ind,vector<int>& cost,vector<int>& dp){
-        if(ind==0) return cost[0];
-        if(ind==1) return cost[1];
-        if(dp[ind]!=-1) return dp[ind];
-        int left=cost[ind]+func(ind-1,cost,dp);
-        int right=INT_MAX;
-        if(ind>=2){
-            right=cost[ind]+func(ind-2,cost,dp);
-        }
-        return dp[ind]=min(right,left);
-    }
+    // int func(int ind,vector<int>& cost,vector<int>& dp){
+    //     if(ind==0) return cost[0];
+    //     if(ind==1) return cost[1];
+    //     if(dp[ind]!=-1) return dp[ind];
+        
+    // }
 public:
     int minCostClimbingStairs(vector<int>& cost) {
 
         int n=cost.size();
-        vector<int> dp(n+1,-1);
-        return min(func(n-1,cost,dp),func(n-2,cost,dp));
+        vector<int> dp(n+1,0);
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int ind=2;ind<n;ind++){
+            int left=cost[ind]+dp[ind-1];
+            int right=INT_MAX;
+            if(ind>=2){
+                right=cost[ind]+dp[ind-2];
+            }
+            dp[ind]=min(right,left);
+        }
+        return min(dp[n-1],dp[n-2]);
     }
 };
